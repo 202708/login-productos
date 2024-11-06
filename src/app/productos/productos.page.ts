31290f563a43ae11eb0c26fc226ef75520635d60
Bryan Concha <b.concha@duocuc.cl>
2024-11-06 19:06:52 -0300

    Se actualiza pagina de productos y corrige encabezado de autorizacion

--- src/app/productos/productos.page.ts
+++ src/app/productos/productos.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../servicio/producto/producto.service'; 
+import { Producto } from '../interfaces/Producto'; 
+import { ViewWillEnter, ViewDidLeave } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.page.html',
   styleUrls: ['./productos.page.scss'],
 })
-export class ProductosPage implements OnInit {
+export class ProductosPage implements ViewWillEnter, ViewDidLeave {
+  public productos: Producto[] = [];
+  private subProducto!: Subscription;
 
-  constructor() { }
+  constructor(
+    private prdS: ProductoService,
+  ) { 
 
-  ngOnInit() {
   }
+
+ ionViewDidLeave(): void {
+  if(this.subProducto){
+    this.subProducto.unsubscribe();
+  }
+   
+ }
+
+ ionViewWillEnter(): void {
+  this.subProducto = this.prdS.producto.subscribe(productos => {
+    this.productos = productos;
+  });
+  this.prdS.listarProductos();
+  
+   
+ }
 
 }
